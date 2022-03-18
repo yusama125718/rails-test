0a31c8854a14272aaae35e2b8211c7ee2934d2ed
yusama125718 <yuheisan0329@gmail.com>
2022-03-18 17:02:36 +0900

    勉強中

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,19 @@
 class TopController < ApplicationController
   def index
   end
+  def about
+  end
+  def name
+    render plain: "こんにちは。#{params[:p]}さん"
+  end
+  def redirect
+    flash[:flash1] = "page1に動いたよ。"
+    redirect_to action: "page1"
+  end
+  def page1
+    render plain: "#{flash[:flash1]}そしてここはpage1"
+  end
+  def sum
+    @answer = params[:x].to_i + params[:y].to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "top#index"
+  get "about" => "top#about", as: "about"
+  get "name/:p" => "top#name", as: "name"
+  get "redirect" => "top#redirect", as: "redirect"
+  get "page1" => "top#page1", as: "page1"
+  get "sum/:x/:y" => "top#sum", as: "sum"
 end
